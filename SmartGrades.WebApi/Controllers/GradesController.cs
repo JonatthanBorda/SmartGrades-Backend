c9c41e5fc26c24a24d49e598360785408fec4c83
Jonathan Borda <stivborda@gmail.com>
2025-06-09 16:56:51 -0500

    Implementación de endpoint para filter/profesores.

---
+++ SmartGrades.WebApi/Controllers/GradesController.cs
@@ -0,0 +1,103 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SmartGrades.Application.DTOs.Nota;
+using SmartGrades.Application.Interfaces;
+using SmartGrades.Application.Services;
+using SmartGrades.Domain.Entities;
+
+namespace SmartGrades.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GradesController : ControllerBase
+    {
+        private readonly IGradeService _gradeService;
+        private readonly IMapper _mapper;
+
+        public GradesController(IGradeService gradeService, IMapper mapper)
+        {
+            _gradeService = gradeService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var grades = await _gradeService.GetAllAsync();
+            var gradesDTO = _mapper.Map<IEnumerable<GradeDTO>>(grades);
+            return Ok(gradesDTO);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var grade = await _gradeService.GetByIdAsync(id);
+            if (grade == null) return NotFound();
+            var gradeDTO = _mapper.Map<GradeDTO>(grade);
+            return Ok(gradeDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] GradeCreateDTO gradeCreateDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var grade = _mapper.Map<Grade>(gradeCreateDTO);
+            await _gradeService.AddAsync(grade);
+            var gradeResult = _mapper.Map<GradeDTO>(grade);
+            return CreatedAtAction(nameof(Get), new { id = grade.Id }, gradeResult);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] GradeDTO gradeDTO)
+        {
+            if (id != gradeDTO.Id)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var grade = _mapper.Map<Grade>(gradeDTO);
+            await _gradeService.UpdateAsync(grade);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _gradeService.DeleteAsync(id);
+            return NoContent();
+        }
+
+        [HttpGet("ByStudent/{idStudent}")]
+        public async Task<IActionResult> GetNotasByEstudiante(int idStudent)
+        {
+            var grades = await _gradeService.GetNotasByEstudianteAsync(idStudent);
+            var dtoGrades = _mapper.Map<IEnumerable<GradeDTO>>(grades);
+            return Ok(dtoGrades);
+        }
+
+        [HttpGet("ByTeacher/{idTeacher}")]
+        public async Task<IActionResult> GetNotasByProfesor(int idTeacher)
+        {
+            var grades = await _gradeService.GetNotasByProfesorAsync(idTeacher);
+            var dtoGrades = _mapper.Map<IEnumerable<GradeDTO>>(grades);
+            return Ok(dtoGrades);
+        }
+
+        [HttpGet("filter")]
+        public async Task<IActionResult> Filter(
+        [FromQuery] string? name,
+        [FromQuery] int? idStudent,
+        [FromQuery] int? idTeacher,
+        [FromQuery] string? orderBy = "Id",
+        [FromQuery] bool desc = false,
+        [FromQuery] int page = 1,
+        [FromQuery] int pageSize = 10)
+        {
+            var result = await _gradeService.GetFilteredAsync(name, idStudent, idTeacher, orderBy, desc, page, pageSize);
+            return Ok(result);
+        }
+    }
+}
---
+++ SmartGrades.WebApi/Controllers/TeacherController.cs
@@ -0,0 +1,85 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SmartGrades.Application.DTOs.Profesor;
+using SmartGrades.Application.Interfaces;
+using SmartGrades.Application.Services;
+using SmartGrades.Domain.Entities;
+
+namespace SmartGrades.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TeacherController : ControllerBase
+    {
+        private readonly ITeacherService _teacherService;
+        private readonly IMapper _mapper;
+
+        public TeacherController(ITeacherService teacherService, IMapper mapper)
+        {
+            _teacherService = teacherService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var teachers = await _teacherService.GetAllAsync();
+            var teachersDTO = _mapper.Map<IEnumerable<TeacherDTO>>(teachers);
+            return Ok(teachersDTO);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var teacher = await _teacherService.GetByIdAsync(id);
+            if (teacher == null) return NotFound();
+            var teacherDTO = _mapper.Map<TeacherDTO>(teacher);
+            return Ok(teacherDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] TeacherCreateDTO teacherCreateDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var teacher = _mapper.Map<Teacher>(teacherCreateDTO);
+            await _teacherService.AddAsync(teacher);
+            var teacherResult = _mapper.Map<TeacherDTO>(teacher);
+            return CreatedAtAction(nameof(Get), new { id = teacher.Id }, teacherResult);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] TeacherDTO teacherDTO)
+        {
+            if (id != teacherDTO.Id)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var teacher = _mapper.Map<Teacher>(teacherDTO);
+            await _teacherService.UpdateAsync(teacher);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _teacherService.DeleteAsync(id);
+            return NoContent();
+        }
+
+        [HttpGet("filter")]
+        public async Task<IActionResult> Filter(
+        [FromQuery] string? name,
+        [FromQuery] string? orderBy = "Id",
+        [FromQuery] bool desc = false,
+        [FromQuery] int page = 1,
+        [FromQuery] int pageSize = 10)
+        {
+            var result = await _teacherService.GetFilteredAsync(name, orderBy, desc, page, pageSize);
+            return Ok(result);
+        }
+    }
+}
